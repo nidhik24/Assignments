87aad2f73fb188bfa5c15b717e5e398fed98d061
nidhik24 <40626111+nidhik24@users.noreply.github.com>
2018-07-05 20:56:33 +0530

    Update Exercise1.c

--- Assignment6/Exercise1.c
+++ Assignment6/Exercise1.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
-extern int getline(char [], int);
+int getline1(char [], int);
 
 int main()
 {
 	char line[100];
 	char *pat = "hello";
 
-	while(getline(line, 100) != EOF)
+	while(getline1(line, 100) != EOF)
 		{
 		if(strstr(line, pat) != NULL)
 			printf("%s\n", line);
 		}
 
 	return 0;
+}
+
+int getline1(char line[], int max)
+{
+int nch = 0;
+int c;
+max = max - 1;	
+while((c = getchar()) != EOF)
+	{
+	if(c == '\n')
+		break;
+	if(nch < max)
+		{
+		line[nch] = c;
+		nch = nch + 1;
+		}
+	}
+if(c == EOF && nch == 0)
+  {
+	return EOF;
+  }
+line[nch] = '\0';
+return nch;
 }
